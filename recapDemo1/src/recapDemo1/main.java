9e16c9b75fc347193ab14d6e70f21427c45bccbf
kenanerin <99602952+kenanerin@users.noreply.github.com>
2022-09-20 15:11:56 +0300

    Add files via upload

---
+++ recapDemo1/src/recapDemo1/main.java
@@ -0,0 +1,22 @@
+package recapDemo1;
+
+public class main {
+
+	public static void main(String[] args) {
+		int sayi1=20;
+		int sayi2=50;
+		int sayi3=51;
+		int enBuyuk = sayi1;
+		if(enBuyuk < sayi2)
+		{
+			enBuyuk=sayi2;
+		}
+		if(enBuyuk <sayi3)
+		{
+			enBuyuk=sayi3;
+		}
+			System.out.println("En Büyük Sayi : " + enBuyuk);
+			
+	}
+
+}
