d171443a211c9c4b1af72507518fc44b2bc47fdd
kenanerin <99602952+kenanerin@users.noreply.github.com>
2022-09-20 15:12:44 +0300

    Add files via upload

---
+++ sesliHarfler/src/sesliHarfler/main.java
@@ -0,0 +1,23 @@
+package sesliHarfler;
+
+public class main {
+
+	public static void main(String[] args) {
+		char harf = 'E';
+		
+		switch(harf)
+		{
+		case 'A':
+		case 'I':
+		case 'O':
+		case 'U':
+			System.out.println("Kalın Sesli Harf");
+			break;
+		default: 
+			System.out.println("İnce Sesli Harf");
+		
+		}
+
+	}
+
+}
