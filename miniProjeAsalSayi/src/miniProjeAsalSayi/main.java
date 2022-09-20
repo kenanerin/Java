653bddd75766f8918e129295da136d958e5a3913
kenanerin <99602952+kenanerin@users.noreply.github.com>
2022-09-20 15:11:19 +0300

    Add files via upload

---
+++ miniProjeAsalSayi/src/miniProjeAsalSayi/main.java
@@ -0,0 +1,39 @@
+package miniProjeAsalSayi;
+
+public class main {
+
+	public static void main(String[] args) {
+		int number =1;
+		int remainder = number % 2;
+		//System.out.println(remainder);
+		boolean isPrime= true;
+		
+		if(number==1)
+		{
+			System.out.println("Sayı Asal Değildir");
+			return;
+		}
+		
+		if (number<2)
+		{
+			System.out.println("Geçersiz Sayı");
+		}
+		for(int i=2; i<number;i++)
+		{
+			if(number % i ==0)
+			{
+				isPrime= false;
+			}
+		}
+		if(isPrime)
+		{
+			System.out.println("Sayi Asaldır");
+			
+		}else {
+			System.out.println("Sayi Asal Değildir");
+		}
+		
+		
+	}
+
+}
